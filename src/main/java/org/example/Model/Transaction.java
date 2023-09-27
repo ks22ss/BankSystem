924c297e17e76e7fa804524a6be36598fcf38f0f
Administrator <admin@example.com>
2023-09-28 00:11:45 +0800

    Transaction initilize, id as uuid, also added transaction type

--- src/main/java/org/example/Model/Transaction.java
+++ src/main/java/org/example/Model/Transaction.java
@@ -1,11 +1,24 @@
 package org.example.Model;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class Transaction {
-    private int id;
-    private Date createDate;
-    private String from;
-    private String to;
-    private double amount;
+    private final UUID id;
+    private final Date transactionDate;
+
+    private final TransactionType transactionType;
+    private final String from;
+    private final String to;
+    private final double amount;
+
+    public Transaction(String from, String to, double amount, TransactionType transactionType) {
+        this.id = UUID.randomUUID();
+        this.transactionDate = new Date();
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
 }
